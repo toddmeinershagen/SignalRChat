c361404adf59fc9b4811ee8b4e001eb2705f4224
Todd Meinershagen <todd@meinershagen.net>
2014-08-14 21:53:30 -0500

    Initial commit

---
+++ SignalRChat.Command/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRChat.Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hubConnection = new HubConnection("http://localhost:1602");
+            var chat = hubConnection.CreateHubProxy("ChatHub");
+            chat.On<string, string>("broadcastMessage", DisplayMessage);
+            hubConnection.Start().Wait();
+            chat.Invoke("Notify", "Console app", hubConnection.ConnectionId);
+            string msg = null;
+
+            while ((msg = Console.ReadLine()) != null)
+            {
+                chat.Invoke("Send", "Console app", msg).Wait();
+            }
+        }
+
+        static void DisplayMessage(string name, string message)
+        {
+            Console.Write(name + ": "); Console.WriteLine(message);
+        }
+    }
+}
